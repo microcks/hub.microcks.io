502ade4e2494f43d236b69ed3620685780de8f76
arthur <arthur.broudoux@gmail.com>
2024-06-14 23:19:37 +0200

    feature: fetch and display packages data Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>
    Signed-off-by: arthur <arthur.broudoux@gmail.com>

--- frontend/src/lib/components/packages/SectionPackages.svelte
+++ frontend/src/lib/components/packages/SectionPackages.svelte
@@ -4,12 +4,11 @@
 	import FiltersPackages from '$lib/components/packages/FiltersPackages.svelte';
 	import GridPackages from '$lib/components/packages/GridPackages.svelte';
 
-	export let packagesData: Package[];
-	console.log('SectionPackage.svelte', packagesData);
+	export let packagesData: Package[] = [];
 </script>
 
 {#if !packagesData}
-	<p>Loading...</p>
+	<p>Loading...from section</p>
 {:else}
 	<section class="w-full h-auto flex flex-row gap-4 p-8">
 		<FiltersPackages />
--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,9 +1,9 @@
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
+	const data = await fetch('http://localhost:4000/api/mocks').then((res) => res.json());
+
 	return {
-		data: await fetch('http://localhost:4000/api/mocks')
-			.then((res) => res.json())
-			.then((data) => data)
+		data
 	};
 };
