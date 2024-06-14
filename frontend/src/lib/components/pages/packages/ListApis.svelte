4ee39f63d0d3a4788dc3c71c11b30ace729d1a45
arthur <arthur.broudoux@gmail.com>
2024-06-15 00:21:50 +0200

    feat: create detailed apis & packages types Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- frontend/src/lib/components/pages/packages/ListApis.svelte
+++ frontend/src/lib/components/pages/packages/ListApis.svelte
@@ -3,17 +3,17 @@
 
 	import CardApi from '$lib/components/pages/packages/CardApi.svelte';
 
-	export let apiList: API[];
+	export let apisData: API[];
 	export let pkg: Package;
 </script>
 
-{#if !apiList || !pkg}
+{#if !apisData || !pkg}
 	<p>Loading...from ListApis</p>
 {:else}
-	<div class="w-1/4 flex flex-col items-start">
+	<div class="w-2/5 flex flex-col items-start">
 		<h2 class="text-lg mb-4">Available APIs</h2>
 		<ul class="flex flex-col items-start gap-2 w-full">
-			{#each apiList as api (api.name)}
+			{#each apisData as api (api.name)}
 				<CardApi {pkg} {api} />
 			{/each}
 		</ul>
--- frontend/src/routes/package/[pkj_name]/+page.server.ts
+++ frontend/src/routes/package/[pkj_name]/+page.server.ts
@@ -2,9 +2,15 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
 	const namePkg = params.pkj_name ? `/${params.pkj_name}` : '';
-	const data = await fetch(`http://localhost:4000/api/mocks${namePkg}`).then((res) => res.json());
+	const packageData = await fetch(`http://localhost:4000/api/mocks${namePkg}`).then((res) =>
+		res.json()
+	);
+	const apisData = await fetch(`http://localhost:4000/api/mocks${namePkg}/apis`).then((res) =>
+		res.json()
+	);
 
 	return {
-		data
+		packageData,
+		apisData
 	};
 };
