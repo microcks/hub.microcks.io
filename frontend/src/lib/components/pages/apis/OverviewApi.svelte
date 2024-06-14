758e75b0e11a4f6e667650c014564a317b826105
arthur <arthur.broudoux@gmail.com>
2024-06-15 01:40:51 +0200

    feature: improve api overview Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>
    Signed-off-by: arthur <arthur.broudoux@gmail.com>

--- frontend/src/lib/components/pages/apis/OverviewApi.svelte
+++ frontend/src/lib/components/pages/apis/OverviewApi.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
-	import Button from '$lib/components/ui/button/button.svelte';
-	import type { API } from '#/types';
+	import type { APIDetails, PackageDetails } from '#/types';
 
-	export let api: API;
+	import Button from '$lib/components/ui/button/button.svelte';
+
+	export let pkg: PackageDetails;
+	export let api: APIDetails;
 </script>
 
-<div class="w-1/4 flex flex-col items-start gap-4">
+<div class="w-2/5 flex flex-col items-start gap-4">
 	<Button class="w-full">Install</Button>
-	<div class="w-full bg-muted p-2 rounded">
+	<div class="w-full bg-muted p-4 rounded flex flex-col items-start gap-2">
 		<p class="text-muted-foreground">Version: <span class="text-foreground">{api.version}</span></p>
+		<p class="text-muted-foreground">
+			Provider: <span class="text-foreground">{pkg.provider}</span>
+		</p>
+		<p class="text-muted-foreground">
+			Contract: <a class="text-foreground" href={api.contracts.url}>{api.contracts.type}</a>
+		</p>
+		<p class="text-muted-foreground">
+			Created At: <span class="text-foreground">{api.createdAt}</span>
+		</p>
 	</div>
 </div>
