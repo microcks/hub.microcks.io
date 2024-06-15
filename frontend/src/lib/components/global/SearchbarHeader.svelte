92e07092982c9b7e03492c3b4fcf1650dfdd34cb
arthur <arthur.broudoux@gmail.com>
2024-06-15 15:03:21 +0200

    feature: searchbar icons Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>
    Signed-off-by: arthur <arthur.broudoux@gmail.com>

--- frontend/src/lib/components/global/SearchbarHeader.svelte
+++ frontend/src/lib/components/global/SearchbarHeader.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
 	import Search from 'lucide-svelte/icons/search';
+	import Command from 'lucide-svelte/icons/command';
 
 	import Input from '$lib/components/ui/input/input.svelte';
 </script>
 
-<Input placeholder="Search Microcks Hub..." size={48} />
+<div class="relative flex flex-row items-center text-muted-foreground">
+	<Input placeholder="Search Microcks Hub..." size={48} class="w-full pl-10 text-muted" />
+	<Search class="absolute top-1/2 left-4 transform -translate-y-1/2" size={16} />
+	<div
+		class="absolute top-1/2 right-4 flex flex-row items-center justify-center text-muted-foreground transform -translate-y-1/2"
+	>
+		<Command size={16} />
+		K
+	</div>
+</div>
