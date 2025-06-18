d4e0d0ed0cd88cf6212138a482ce2959481d63af
Paul PLANCQ <paul.plancq@outlook.fr>
2025-06-18 18:43:48 +0200

    refactor: restructure app to use custom elements and improve routing
    Signed-off-by: Paul PLANCQ <paul.plancq@outlook.fr>

--- react/src/App/App.tsx
+++ react/src/App/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter } from '@/App/Routing/BrowserRouter.tsx';
 
+import '@/assets/css';
+
 export const App = () =>  {
   return (
     <BrowserRouter />
---
+++ react/src/App/AppHTMLElement.tsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react';
+import { createRoot, type Root } from 'react-dom/client';
+import { App } from './App';
+
+export class AppHTMLElement extends HTMLElement {
+  private readonly root: Root;
+
+  constructor() {
+    super();
+
+    this.root = createRoot(this);
+  }
+
+  connectedCallback() {
+    this.root.render(
+      <StrictMode>
+        <App />
+      </StrictMode>
+    );
+  }
+
+  disconnectedCallback() {
+    queueMicrotask(() => this.root.unmount());
+  }
+}
